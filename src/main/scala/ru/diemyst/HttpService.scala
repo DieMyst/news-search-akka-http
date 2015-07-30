0f8969be5e9471c92d5641df5270e75afe4c0765
dshakhtarin <dshakhtarin@at-consulting.ru>
2015-07-30 19:21:07 +0300

    partly

--- src/main/scala/ru/diemyst/HttpService.scala
+++ src/main/scala/ru/diemyst/HttpService.scala
@@ -4,29 +4,32 @@ import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.{HttpRequest, StatusCodes, Uri}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Future, ExecutionContext}
 import ExecutionContext.Implicits.global
 
 import scala.concurrent.duration._
 import spray.json.DefaultJsonProtocol
 
 /**
- * Created by dshakhtarin on 30.07.2015.
+ * Created on 30.07.2015.
+ * News search from Google API
  */
-case class News(news: List[Query])
+case class Request(news: List[Query])
 case class Query(query: String, n: Int)
 case class Response(date: String, result: Seq[References])
 case class References(site: String, n: Int)
 
 object Protocols extends DefaultJsonProtocol with SprayJsonSupport{
   implicit def queryFormat = jsonFormat2(Query.apply)
-  implicit def newsFormat = jsonFormat1(News.apply)
+  implicit def newsFormat = jsonFormat1(Request.apply)
 }
 
 class HttpService(implicit system: ActorSystem) {
@@ -42,9 +45,19 @@ class HttpService(implicit system: ActorSystem) {
 
   val route: Route = {
     pathPrefix("analyzenews") {
-      (post & entity(as[News])) { news =>
+      (post & entity(as[Request])) { request =>
         complete {
-          println(news)
+          println(request)
+          val splits = request.news.map(q => (q.query.split(" ").map(str => ("q", str)), q.n))
+          val listOfFutures = splits.map { pair =>
+            val link = Uri("https://ajax.googleapis.com/ajax/services/search/news?v=1.0").withQuery(pair._1 :+("v", "1.0"): _*)
+            println(link)
+            Http().singleRequest(HttpRequest(GET, link))
+              .flatMap(response => Unmarshal(response.entity).to[String])
+              .map(println)
+          }
+          Future.sequence(listOfFutures)
+
           StatusCodes.OK
         }
       }
